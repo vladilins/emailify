3ea0868be7702b4d7b483e5dc9606fa6a4d80b68
Your Name <ilin96inbox@gmail.com>
2018-12-22 02:00:34 +0800

    Survey in User

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredit = require("../middlewares/requireCredite");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 const Survey = mongoose.model("surveys");
 module.exports = app => {
@@ -17,5 +19,7 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
+
+    const mailer = new Mailer(survey, surveyTemplate);
   });
 };
