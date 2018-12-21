a64185db4b41268bf77efb3a85d2f2f79963dac8
Your Name <ilin96inbox@gmail.com>
2018-12-22 01:47:32 +0800

    Send Grid setup

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,21 @@
+const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredit = require("../middlewares/requireCredite");
+
+const Survey = mongoose.model("surveys");
 module.exports = app => {
-  app.post("/api/serveys", requireLogin, requireCredit, (req, res) => {});
+  app.post("/api/serveys", requireLogin, requireCredit, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(",").map(email => ({
+        email: email.trim()
+      })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+  });
 };
