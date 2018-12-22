6c35a94ecbbe9414737cf5532e36478eef9693bd
Your Name <ilin96inbox@gmail.com>
2018-12-23 03:26:39 +0800

    exclude recipients from fetch

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,7 +10,9 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 module.exports = app => {
   app.get("/api/surveys", requireLogin, async (req, res) => {
-    const surveys = await Survey.find({ _user: req.user.id });
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false
+    });
 
     res.send(surveys);
   });
