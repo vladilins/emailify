8819d6f99e1f0a68255d339a4fbc2a4cb852a931
Your Name <ilin96inbox@gmail.com>
2018-12-22 16:17:10 +0800

    Form styling

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 import _ from "lodash";
+import { Link } from "react-router-dom";
 import SurveyField from "./SurveyField";
 
 const FIELDS = [
@@ -14,7 +15,13 @@ class SurveyForm extends Component {
   renderFields() {
     return _.map(FIELDS, ({ label, name }) => {
       return (
-        <Field component={SurveyField} type="text" label={label} name={name} />
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
       );
     });
   }
@@ -23,7 +30,12 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );
