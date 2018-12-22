5f803771cfdf1dd9481291ef488e569b1dcb5ccc
Your Name <ilin96inbox@gmail.com>
2018-12-22 19:08:56 +0800

    display filds

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,21 @@
 import React from "react";
+import _ from "lodash";
 import { connect } from "react-redux";
+import formFields from "./formFIelds";
 
 const SurveyFormReview = ({ onCancel, formValues }) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
   return (
     <div>
       <h5>Please confir your entries</h5>
-
+      {reviewFields}
       <button className="yellow darken-3 btn-flat" onClick={onCancel}>
         Back
       </button>
